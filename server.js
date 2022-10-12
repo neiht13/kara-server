38a60498ec8c18a798eae57994fd4b9f96ca0dc9
neiht13 <thienphan9612@gmail.com>
2022-10-12 15:21:29 +0700

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({path: "./config.env"});
 const port = process.env.PORT || 5005;
 // app.use(cors());
 app.use(express.json({limit: '50mb'}));
@@ -11,29 +11,49 @@ app.use(require("./db/crud"));
 // get driver connection
 const dbo = require("./db/conn");
 app.get("/vercel", (req, res) => {
-  res.send("Express on Vercel");
+    res.send("Express on Vercel");
 });
 app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
+    // perform a database connection when server starts
+    dbo.connectToServer(function (err) {
+        if (err) console.error(err);
 
-  });
-  console.log(`Server is running on port: ${port}`);
+    });
+    console.log(`Server is running on port: ${port}`);
 });
 
 const ObjectId = require("mongodb").ObjectId;
 const dbName = "employees";
+const {MongoClient} = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+var _db;
+client.connect(function (err, db) {
+    // Verify we got a good "db" object
+    if (db) {
+        _db = db.db("employees");
+        console.log("Successfully connected to MongoDB.");
+    }
+    return callback(err);
+});
+const getDb = () => {
+    return _db;
+}
+
 
 app.get("/karaoke", (req, res) => {
-    let db_connect = dbo.getDb(dbName);
+    let db_connect = getDb();
     db_connect
         .collection("karaoke")
         .find({})
         .toArray(function (err, result) {
-          if (err) throw err;
-          res.json(result);
+            if (err) throw err;
+            res.json(result);
         });
-  });
+});
 
 
