4dbdc5fc58df294cbbe5df4d85178d9ba05ab9b7
neiht13 <thienphan9612@gmail.com>
2022-10-12 11:27:35 +0700

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5005;
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
