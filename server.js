a5f2881239b7f43a7237029118f9d984a3b4a92b
neiht13 <thienphan9612@gmail.com>
2022-10-12 19:31:59 +0700

    commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({path: "./config.env"});
-const port = process.env.PORT || 5005;
+const port = process.env.PORT || 5000;
 // app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
@@ -13,41 +13,26 @@ const dbo = require("./db/conn");
 app.get("/vercel", (req, res) => {
     res.send("Express on Vercel");
 });
-app.listen(port, () => {
-    // perform a database connection when server starts
-    dbo.connectToServer(function (err) {
-        if (err) console.error(err);
-
-    });
-    console.log(`Server is running on port: ${port}`);
-});
+// app.listen(port, () => {
+//     // perform a database connection when server starts
+//     dbo.connectToServer(function (err) {
+//         if (err) console.error(err);
+//
+//     });
+//     console.log(`Server is running on port: ${port}`);
+// });
 
 const ObjectId = require("mongodb").ObjectId;
 const dbName = "employees";
 const {MongoClient} = require("mongodb");
-const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
-var _db;
-client.connect(function (err, db) {
-    // Verify we got a good "db" object
-    if (db) {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB.");
-    }
-});
-const getDb = () => {
-    return _db;
-}
+const Db = "mongodb+srv://neiht13:hj6scsg4@cluster0.tmy5xk2.mongodb.net?retryWrites=true&w=majority";
+const client = new MongoClient(Db);
 
 
-app.get("/karaoke", (req, res) => {
-    let db_connect = getDb();
-    db_connect
-        .collection("karaoke")
+app.get("/karaoke", async (req, res) => {
+    await client
+        .db('employees')
+        .collection('karaoke')
         .find({})
         .toArray(function (err, result) {
             if (err) throw err;
